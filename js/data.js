34cc75c9fb367c23c0e63c6d79b09c8397a358e1
InozaAki <axelespinoza887@gmail.com>
2023-12-30 20:16:46 -0600

    First commit

---
+++ js/data.js
@@ -0,0 +1,15 @@
+let songs = [
+    {
+        name: 'Lost in the City Lights',
+        path: '../music/lost-in-city-lights-145038.mp3',
+        artist: 'Cosmo Sheldrake',
+        cover: '../images/cover-1.png'
+    },
+    {
+        name: 'Forest Lullaby',
+        path: '../music/lost-in-city-lights-145038.mp3',
+        artist: 'Lesfm',
+        cover: '../images/cover-2.png',
+
+    }
+]
